e6d46cc3bbe63de392babaf911f05532a9aef761
Felix Touchte Codjo <felixtouchtecodjo@gmail.com>
2025-11-10 23:48:56 +0100

    improve visualisation kalman filter steps monitoring

--- analysis/kalman-filter/kfmon.cpp
+++ analysis/kalman-filter/kfmon.cpp
@@ -87,14 +87,16 @@ int main(int argc, char const *argv[]) {
             }
         }
     }
-    gr_ahdcView->SetTitle(TString::Format("AHDC View at z = %.2lf; x(mm); y(mm)", zpos).Data());
+    gr_ahdcView->SetTitle(TString::Format("AHDC XY view at z = %.2lf; x(mm); y(mm)", zpos).Data());
     gr_ahdcView->SetMarkerStyle(4);
     gr_ahdcView->SetMarkerColorAlpha(kBlue-10, 0.3);
     TCanvas * c0 = new TCanvas("c0", "c0 ahdcView", 1000, 1000);
     c0->cd();
     gr_ahdcView->Draw("AP");
     TFile *f = new TFile(output, "RECREATE");
-    c0->Write("ahdcView_empty"); 
+    c0->Write("ahdcView_empty");
+    // color palette
+    //std::vector<int> ColorPalette= {};
 
     // Loop over events
     while( reader.next()){
@@ -119,13 +121,16 @@ int main(int argc, char const *argv[]) {
         // loop over track
         for (int trackRow = 0; trackRow < trackBank.getRows(); trackRow++) {
             // Processing
-            TCanvas * cn = new TCanvas(TString::Format("c%d_%d", (int) nevents, trackRow+1).Data(),TString::Format("c%d_%d ahdcView", (int) nevents, trackRow+1).Data(), 1000, 1000);
-            cn->cd();
-            // Draw AHDC view
-            gr_ahdcView->Draw("AP");
+            TCanvas * cn = new TCanvas(TString::Format("c%d_%d", (int) nevents, trackRow+1).Data(),TString::Format("c%d_%d ahdcView", (int) nevents, trackRow+1).Data(), 1600, 900);
+            cn->Divide(2,1);
+            // XY view
+            cn->cd(1);
+            gr_ahdcView->Draw("AP"); // AHDC view
             int trackid = trackBank.getInt("trackid", trackRow);
-            // Loop over hits
+                // Loop over hits
             TGraph * gr_hits = new TGraph();
+            gr_hits->SetMarkerColor(2);
+            gr_hits->SetMarkerStyle(8);
             for (int i = 0; i < hitBank.getRows(); i++) {
                 if (hitBank.getInt("trackid", i) != trackid) continue;
                 int sl = hitBank.get("superlayer", i)-1;
@@ -135,11 +140,32 @@ int main(int argc, char const *argv[]) {
                 wire->set_z(zpos);
                 gr_hits->AddPoint(wire->x, wire->y);
             }
-            gr_hits->SetMarkerColor(2);
-            gr_hits->SetMarkerStyle(8);
             gr_hits->Draw("P");
-            // key : Niter, orientation, status --> TGraph*
-            std::map<std::tuple<int, int, int>, TGraph*> graphMap;
+            // ZR view
+                // define the frame
+            TGraph * gr_zrView = new TGraph();
+            gr_zrView->SetTitle("ZR view; z (mm); r (mm)");
+            gr_zrView->AddPoint(-150,-5);
+            gr_zrView->AddPoint(-150,70);
+            gr_zrView->AddPoint(150,70);
+            gr_zrView->AddPoint(150,-5);
+            cn->cd(2);
+            gr_zrView->Draw("AP");
+            //gr_zrView->SetLineColor(kBlue-10);
+                // draw nivel line
+            for (auto radius : std::vector<double>({0, 3.0, 3.06, 32, 38, 38+4, 48, 48+4, 58, 58+4, 68})) {
+                TGraph * gr = new TGraph();
+                gr->AddPoint(-150,radius);
+                gr->AddPoint(150,radius);
+                gr->SetLineColor(kBlue-10);
+                gr->SetLineStyle(2);
+                //gr->SetLineWidth(2);
+                gr->Draw("same l");
+            }
+            // key : Niter, orientation, status --> TGraph* to draw markers
+            std::map<std::tuple<int, int, int>, std::pair<TGraph*,TGraph*>> graphMap;
+            // key : Niter to draw lines
+            std::map<int, std::pair<TGraph*,TGraph*>> graphMapIter;
             for (int i = 0; i < kfmonBank.getRows(); i++) {
                 if (kfmonBank.getInt("trackid", i) != trackid) continue;
                 int Niter = kfmonBank.getShort("Niter", i);
@@ -148,65 +174,110 @@ int main(int argc, char const *argv[]) {
                 int status = kfmonBank.getShort("status", i);
                 double x = kfmonBank.getFloat("x", i);
                 double y = kfmonBank.getFloat("y", i);
-                //double z = kfmonBank.getFloat("z", i);
+                double z = kfmonBank.getFloat("z", i);
                 //double px = kfmonBank.getFloat("px", i);
                 //double py = kfmonBank.getFloat("py", i);
                 //double pz = kfmonBank.getFloat("pz", i);
                 
-                // it : iterator, it is an std::pair containing 
+                // it : iterator, is an object that points to a std::pair containing 
                 // it->first is the key, here the std::tuple
                 // it->second is the value, here the TGraph*
                 // inserted is a boolean, is false if the element already exist and true if it is a new element
                 // if false, "it" corresponds to the existing element
                 
-                //if (Niter > 2) continue;
-                auto [it, inserted] = graphMap.insert({std::make_tuple(Niter, orientation, status), new TGraph()});
-                it->second->AddPoint(x,y);
+                if (Niter >= 4) continue;
+                { // Niter, orientation, status
+                    auto [it, inserted] = graphMap.insert({std::make_tuple(Niter, orientation, status), std::make_pair(new TGraph(), new TGraph())});
+                    (it->second).first->AddPoint(x,y); // xy
+                    (it->second).second->AddPoint(z,sqrt(x*x+y*y)); //zr
+                }
+                { // Niter only
+                    auto [it, inserted] = graphMapIter.insert({Niter, std::make_pair(new TGraph(), new TGraph())});
+                    (it->second).first->AddPoint(x,y); // xy
+                    (it->second).second->AddPoint(z,sqrt(x*x+y*y)); //zr
+                }
             }
-            // set graph style
             TLegend* legend = new TLegend(0.1,0.2,0.3,0.9);
+            // Line style 
+            for (auto& [k, g] : graphMapIter) {
+                // Set Color
+                // they should be the same as the marker
+                int Niter = k;
+                if (Niter <= 8) {
+                    g.first->SetLineColor(Niter+1); 
+                    g.second->SetLineColor(Niter+1);
+                }
+                else { // Niter > 9
+                       // we ship the kColor 10 as it is transparente
+                    g.first->SetLineColor(Niter+2);
+                    g.second->SetLineColor(Niter+2);
+                }
+                // Draw line
+                cn->cd(1);
+                g.first->Draw("same L");
+                cn->cd(2);
+                g.second->Draw("same L");
+            }
+            // Marker style
             for (auto& [k, g] : graphMap) {
                 auto [Niter, orientation, status] = k; // key
                 // Set Color
-                if (Niter < 8) {
-                    g->SetLineColor(Niter+1);
-                    g->SetMarkerColor(Niter+1);
+                if (Niter <= 8) {
+                    g.first->SetLineColor(Niter+1);
+                    g.second->SetLineColor(Niter+1);
+                    g.first->SetMarkerColor(Niter+1);
+                    g.second->SetMarkerColor(Niter+1);
                 }
-                else { // Niter >= 9
+                else { // Niter > 9
                        // we ship the kColor 10 as it is transparente
-                    g->SetLineColor(Niter+2);
-                    g->SetMarkerColor(Niter+2);
+                    g.first->SetLineColor(Niter+2);
+                    g.second->SetLineColor(Niter+2);
+                    g.first->SetMarkerColor(Niter+2);
+                    g.second->SetMarkerColor(Niter+2);
                 }
                 // Set Marker style
+                g.first->SetMarkerSize(2);
+                g.second->SetMarkerSize(2);
                 if ((orientation % 2 == 0) && (status == 0)) {
                     // forward or postfit propagation and prediction
-                    g->SetMarkerStyle(26);
-                    legend->AddEntry(g, TString::Format("Niter : %d, %s, %s", Niter, "forward", "prediction").Data());
+                    g.first->SetMarkerStyle(26);
+                    g.second->SetMarkerStyle(26);
+                    legend->AddEntry(g.first, TString::Format("Niter : %d, %s, %s", Niter, "forward", "prediction").Data());
                 }
                 else if ((orientation % 2 == 0) && (status == 1)) {
                     // forward or postfit propagation and correction
-                    g->SetMarkerStyle(22);
-                    legend->AddEntry(g, TString::Format("Niter : %d, %s, %s", Niter, "forward", "correction").Data());
+                    g.first->SetMarkerStyle(25);
+                    g.second->SetMarkerStyle(25);
+                    legend->AddEntry(g.first, TString::Format("Niter : %d, %s, %s", Niter, "forward", "correction").Data());
                 }
                 else if ((orientation % 2 == 1) && (status == 0)) {
                     // backward propagation and prediction
-                    g->SetMarkerStyle(32);
-                    legend->AddEntry(g, TString::Format("Niter : %d, %s, %s", Niter, "backward", "prediction").Data());
+                    g.first->SetMarkerStyle(32);
+                    g.second->SetMarkerStyle(32);
+                    legend->AddEntry(g.first, TString::Format("Niter : %d, %s, %s", Niter, "backward", "prediction").Data());
                 }
                 else { //if ((orientation % 2 == 0) && (status == 1)) {
                     // backward propagation and correction
-                    g->SetMarkerStyle(23);
-                    legend->AddEntry(g, TString::Format("Niter : %d, %s, %s", Niter, "backward", "correction").Data());
+                    g.first->SetMarkerStyle(24);
+                    g.second->SetMarkerStyle(24);
+                    legend->AddEntry(g.first, TString::Format("Niter : %d, %s, %s", Niter, "backward", "correction").Data());
                 }
-                cn->cd();
-                g->Draw("same PL");
-                //legend->Draw();
+                // XY view
+                cn->cd(1);
+                g.first->Draw("same P");
+                // ZR view
+                cn->cd(2);
+                g.second->Draw("same P");
             }
+            // Draw legend
+            cn->cd(1);
+            legend->Draw();
             // output
             cn->Write(TString::Format("event_%d_track_%d", (int) nevents, trackRow+1).Data());
             // delete graphMap
             for (auto& [k, g] : graphMap) {
-                delete g;
+                delete g.first;
+                delete g.second;
             }
         }
 
