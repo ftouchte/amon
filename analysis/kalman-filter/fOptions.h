ecaa29ea06ec4fa3134d51587b5f40688da13d9d
Felix Touchte Codjo <felixtouchtecodjo@gmail.com>
2026-01-17 23:40:53 +0100

    manage options in c++

---
+++ analysis/kalman-filter/fOptions.h
@@ -0,0 +1,20 @@
+#ifndef F_OPTIONS_H
+#define F_OPTIONS_H
+
+#include <map>
+#include <string>
+#include <vector>
+
+class fOptions {
+    std::vector<std::string> opts;
+    std::map<std::string, std::string> mopts;
+public:
+    fOptions(std::vector<std::string> _opts);
+    void LoadOptions(int argc, const char * argv[]);
+    bool IsOption(std::string str);
+    std::string GetValue(std::string str);
+    void Show();
+    void RunTest(int argc, const char * argv[]);
+};
+
+#endif
