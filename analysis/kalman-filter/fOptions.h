f314b6abb6af3514b43277c4a4973f09db2f4d70
Felix Touchte Codjo <felixtouchtecodjo@gmail.com>
2026-03-22 18:21:58 +0100

    parse more that a argument per option

--- analysis/kalman-filter/fOptions.h
+++ analysis/kalman-filter/fOptions.h
@@ -14,12 +14,55 @@
 
 class fOptions {
     std::vector<std::string> opts;
-    std::map<std::string, std::string> mopts;
+    std::map<std::string, std::vector<std::string>> mopts;
 public:
+    /**
+     * @brief Construct a new fOptions object
+     * 
+     * The user is free to give any sense to the list of options. 
+     * e.g fOptions({"-i", "-o"}) where -i stands for input and -o for output
+     * 
+     * @param _opts list of options
+     */
     fOptions(std::vector<std::string> _opts);
+
+    /**
+     * @brief Load/parse options
+     * 
+     * @param argc same argument as for main()
+     * @param argv same argument as for main()
+     */
     void LoadOptions(int argc, const char * argv[]);
-    bool IsOption(std::string str);
+    
+    /**
+     * @brief Check if str is an option
+     * 
+     * @param str 
+     * @return true str is an option defined at the construction of the object
+     * @return false otherwise
+     */
+     bool IsOption(std::string str);
+    
+     /**
+     * @brief Get the first value of the option
+     * 
+     * @param str 
+     * @return argument for this option
+     */
     std::string GetValue(std::string str);
+
+    /**
+     * @brief Get all values for this option
+     * 
+     * @param str 
+     * @return list of arguments for this option
+     */
+    std::vector<std::string> GetValues(std::string str);
+    
+    /**
+     * @brief Display the value for each option
+     * 
+     */
     void Show();
 };
 
