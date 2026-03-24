47c3ca30b20fe92f7601b2f9919a02540aa748b4
Felix Touchte Codjo <felixtouchtecodjo@gmail.com>
2026-03-24 14:46:46 -0400

    add script

---
+++ scripts/hipo/event-filter-for-whole-run.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+path=/lustre24/expphy/volatile/clas12/touchte/kalman-filter/v120
+outdir=/lustre24/expphy/volatile/clas12/touchte/kalman-filter/elastic-filtered/v120
+
+#for file in $path/rec*
+for file in v120/rec_clas_022712.evio.0000[0-3].hipo
+do
+    echo -e "\033[1;32m > Filter elastics for : \033[0m $file"
+    filename=$(basename "$file") # remove the path
+    #name="${filename%.*}" # remove the extension
+    /w/hallb-scshelf2102/clas12/users/touchte/amon/scripts/hipo/event-filter.sh $file "$outdir/$filename"
+done
