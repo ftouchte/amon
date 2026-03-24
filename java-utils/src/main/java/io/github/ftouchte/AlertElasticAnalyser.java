1a8a92b4b68cb5d8d5da806bc3c3738e0da0a4a6
Felix Touchte Codjo <felixtouchtecodjo@gmail.com>
2026-03-24 14:26:46 -0400

    build elastic event filter for hipo file

---
+++ java-utils/src/main/java/io/github/ftouchte/AlertElasticAnalyser.java
@@ -0,0 +1,118 @@
+package io.github.ftouchte;
+
+import org.jlab.io.base.DataEvent;
+import org.jlab.io.base.DataBank;
+import java.util.ArrayList;
+
+/**
+ * Inspired by amon/analysis/kalman-filter/kfmon_data.cpp
+ * 
+ * For now: the study is made on a deuteron target
+ */
+public class AlertElasticAnalyser {
+    
+    // Constants
+    double beam_energy = 2.23951 * Units.GeV; // incident energy if the electron, GeV
+    double electron_mass = 0.511e-3 * Units.GeV; // energy mass of electron, GeV
+    double proton_mass = 938.272e-3 * Units.GeV; // energy mass of proton, GeV
+    double helium_mass = 3.73 * Units.GeV; // energy mass of Helium-4, GeV
+    double deuteron_mass = 1.875 * Units.GeV; // energy mass of Deuterium, GeV
+
+    // Elatsics cuts
+    double delta_phi = 20;
+    double W2_min = 3.5 * Units.GeV; // GeV
+    double W2_max = 3.8 * Units.GeV; // GeV
+    int nhits_min = 6;
+
+    // Particles
+    ParticleRow electron;
+    ParticleRow ahdc_track;
+
+    public boolean IsElastic(DataEvent event) {
+        if (!event.hasBank("REC::Particle") && !event.hasBank("AHDC::kftrack")) {
+            return false;
+        }
+        
+        DataBank recBank = event.getBank("REC::Particle");
+        DataBank trackBank = event.getBank("AHDC::kftrack");
+
+        electron = null;
+        ahdc_track = null;
+
+        
+        for (int row = 0; row < recBank.rows(); row++) {
+            // Select trigger electrons
+            if (recBank.getInt("pid", row) == 11 && recBank.getShort("status", row) < 0) {
+                // compute kinematic variables
+                double px = recBank.getFloat("px", row);
+                double py = recBank.getFloat("py", row);
+                double pz = recBank.getFloat("pz", row);
+                ParticleRow electron_candidate = new ParticleRow(px * Units.GeV, py * Units.GeV, pz * Units.GeV);
+                // physics kinematics
+                double scattered_beam_energy = Math.sqrt(Math.pow(electron_candidate.p(Units.GeV),2) + Math.pow(electron_mass,2));
+                double nu = beam_energy - scattered_beam_energy;
+                double Q2 = 4*beam_energy*scattered_beam_energy*Math.pow(Math.sin(electron_candidate.theta(Units.rad)/2),2);
+                double W2 = Math.pow(deuteron_mass,2) + 2*deuteron_mass*nu - Q2;
+                // W2 cut to retrieve the target mass
+                if (W2 > W2_min && W2 < W2_max) {
+                    electron = electron_candidate;
+                    // Select the best AHDC track
+                    ArrayList<ParticleRow> all_tracks = new ArrayList<>();
+                    for (int trow = 0; trow < trackBank.rows(); trow++) {
+                        double px1 = trackBank.getFloat("px", trow);
+                        double py1 = trackBank.getFloat("py", trow);
+                        double pz1 = trackBank.getFloat("pz", trow);
+                        ParticleRow track_candidate = new ParticleRow(px1 * Units.MeV, py1 * Units.MeV, pz1 * Units.MeV);
+                        if (trackBank.getInt("n_hits", trow) >= nhits_min) {
+                            all_tracks.add(track_candidate);
+                        }
+                    }
+                    // Sort track accordingly to the smallest delta phi
+                    all_tracks.sort((t1, t2) -> {
+                        double dphi1 = deltaPhi(electron.phi(Units.deg), t1.phi(Units.deg));
+                        double dphi2 = deltaPhi(electron.phi(Units.deg), t2.phi(Units.deg));
+                        return Double.compare(dphi1, dphi2);
+                    });
+                    // Select the best track
+                    if (all_tracks.size() > 0) {
+                        ParticleRow best_track = all_tracks.get(0);
+                        if (deltaPhi(electron.phi(Units.deg), best_track.phi(Units.deg)) < delta_phi) {
+                            ahdc_track = best_track;
+                            return true;
+                        }
+                    }
+                    
+                }
+            }
+        }
+        
+        if (electron != null && ahdc_track != null) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    /**
+     * 
+     * @param electron_phi in deg
+     * @param track_phi in deg
+     */
+    private double deltaPhi(double electron_phi, double track_phi) {
+        return Math.abs(Math.abs(electron_phi - track_phi) - 180);
+    }
+
+    /**
+     * Return elactic electron. Better use after {@link #IsElastic(DataEvent)}
+     */
+    public ParticleRow getElectron() {
+        return this.electron;
+    }
+
+    /**
+     * Return elactic ahdc track. Better use after {@link #IsElastic(DataEvent)}
+     */
+    public ParticleRow getAhdcTrack() {
+        return this.ahdc_track;
+    }
+}
---
+++ java-utils/src/main/java/io/github/ftouchte/ElasticEventFilter.java
@@ -0,0 +1,129 @@
+package io.github.ftouchte;
+
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.jlab.io.hipo.HipoDataEvent;
+import org.jlab.jnp.hipo4.data.Bank;
+import org.jlab.jnp.hipo4.data.Event;
+import org.jlab.jnp.hipo4.data.Schema;
+import org.jlab.jnp.hipo4.data.SchemaFactory;
+import org.jlab.jnp.hipo4.io.HipoReader;
+import org.jlab.jnp.hipo4.io.HipoWriterSorted;
+
+
+public class ElasticEventFilter {
+    public static void main(String[] args) {
+
+        if (args.length < 2) {
+            System.out.println("* error : missing arguments");
+            System.out.println("* require an input file and an output file");
+        }
+
+        // Read input and ouputs file from arguments
+        // String inFile = "/volatile/clas12/touchte/kalman-filter/v120/rec_clas_022712.evio.00000.hipo";
+		// String outFile = "/lustre24/expphy/volatile/clas12/touchte/kalman-filter/test/elastic_" + Path.of(inFile).getFileName().toString();
+        String inFile = args[0];
+		String outFile = args[1];
+
+        
+		Path path = Path.of(outFile);
+        
+        // Check that the input file exists
+        if (Files.exists(Path.of(inFile))) {
+            System.out.println("> Input file : " + inFile);
+		} else {
+            System.out.println("* " + path + " : error file not found");
+            return;
+        }
+        
+        // Check that the provided output file does not exist
+        if (Files.exists(path)) {
+			System.out.println(path + " : error existing file");
+            return;
+		}
+
+        // Initialise HIPO reader for the file
+		HipoReader reader = new HipoReader();
+		reader.open(inFile);
+
+        // Read list of schemas from reader
+		// If you want, you can only select the desired schemas
+		// e.g create an empty List<Schema> and add
+		// list.add(new Schema("AHDC::kftrack", 23000, 26))
+		// in that case, refer to the bank definitions in coatjava
+		SchemaFactory factory = reader.getSchemaFactory();
+		List<Schema>   schemaList   = factory.getSchemaList();
+
+        // Create new (empty) banks from all choosed schemas
+		// The will be loaded/filled during the event process
+	    List<Bank>     schemaBanks  = new ArrayList<Bank>();
+		for (Schema schema : schemaList){
+			schemaBanks.add(new Bank(schema));
+		}
+
+        // Initialise a writer with the selected schemas
+		// used to create a new HIPO file
+		HipoWriterSorted writer = new HipoWriterSorted();
+		for(Schema schema : schemaList){
+				writer.getSchemaFactory().addSchema(schema);
+		}
+		// Open the output file
+		writer.open(outFile);
+
+        // ------------------------------------------
+		// Start analysis : event selection
+		// You can add your own cuts
+		// ------------------------------------------
+		Event   inEvent = new Event();
+		Event   outEvent   = new Event();
+
+        // Initialiase elastic analyser
+        AlertElasticAnalyser analyser = new AlertElasticAnalyser();
+
+        // Loop over events
+        int counter = 0;
+        int nevents = 0;
+        while(reader.hasNext()==true){ // just check if we have a next event but do not load it
+            // print
+            nevents++;
+            if (nevents % 10000 == 0) {
+                System.out.println("  * processed events : " + nevents);
+            }
+            
+            // reset outEvent
+			outEvent.reset();
+
+			// load the event
+			reader.nextEvent(inEvent);
+
+            // Check that the event is an elastic
+            if (analyser.IsElastic(new HipoDataEvent(inEvent, factory))) {
+                counter++;
+                // loop over all existing banks in the input file (reader)
+                for(int b = 0; b < schemaBanks.size(); b++){
+                    // load the corresponding bank as did for runConfigBank
+                    inEvent.read(schemaBanks.get(b));
+                    // write this bank in the output file it has at least one row
+                    if (schemaBanks.get(b).getRows() > 0) {
+                        outEvent.write(schemaBanks.get(b));
+                    }
+                }
+                // add this event to the writer
+                int tag = inEvent.getEventTag();
+                outEvent.setEventTag(tag);
+                writer.addEvent(outEvent,outEvent.getEventTag());
+            }
+
+        } // end loop over events
+
+        // ----------------
+		// Save file
+		// ----------------
+		writer.close();
+        System.out.println("> Output file : " + outFile);
+        System.out.println("> Nb elastics : " + counter);
+    }
+}
