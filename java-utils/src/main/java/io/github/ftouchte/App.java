2b7ca2b27dcdd8d82ec74bc0ff4832a0239436f8
Felix Touchte Codjo <felixtouchtecodjo@gmail.com>
2026-04-10 13:43:11 -0400

    test new methods to compute angles

--- java-utils/src/main/java/io/github/ftouchte/App.java
+++ java-utils/src/main/java/io/github/ftouchte/App.java
@@ -1,5 +1,13 @@
 package io.github.ftouchte;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Random;
+
+import io.github.ftouchte.alignment.AhdcAlignmentAnalyser;
+import io.github.ftouchte.alignment.AhdcWireId;
+
 /**
  * Hello world!
  *
@@ -9,5 +17,20 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+        test(args);
+    }
+
+    public static void test( String[] args ) {
+        int Npts = 10;
+        double[] vector = new double[Npts];
+        Random rand = new Random();
+        for (int i = 0; i < vector.length; i++) {
+            vector[i] = Math.abs(rand.nextInt()) % 1000;
+        }
+
+        int[] sortedIndices = AhdcAlignmentAnalyser.sortIndices(vector, false);
+
+        System.out.println("Vector : " + Arrays.toString(vector));
+        System.out.println("Sorted indices : " + Arrays.toString(sortedIndices));
     }
 }
--- java-utils/src/main/java/io/github/ftouchte/alignment/ResultsOverIterations.java
+++ java-utils/src/main/java/io/github/ftouchte/alignment/ResultsOverIterations.java
@@ -7,7 +7,9 @@ package io.github.ftouchte.alignment;
 public class ResultsOverIterations {
     
     // layers
-    public double[] layer_angles = {0.991, 1.245, 0.708, 1.086, 0.638, 1.066, 0.667, 0.951}; // from the alignment without fit
+    //public double[] layer_angles = {0.991, 1.245, 0.708, 1.086, 0.638, 1.066, 0.667, 0.951}; // layer-alignment-v2 (no fit)
+    //public double[] layer_angles = {0.885, 1.032, 0.647, 0.924, 0.572, 0.771, 0.540, 0.628}; // crystal ball fit v3 (iter 50)
+    public double[] layer_angles = {0.881, 1.023, 0.638, 0.905, 0.545, 0.680, 0.499, 0.567}; // crystal ball fit v4 (iter 15)
     //public double[] layer_angles = {0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0};
     public double[] layer_residuals = {0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0};
 
