7634dd67032fadbf35a6d9e707b7b2343de3e01f
Felix Touchte Codjo <felixtouchtecodjo@gmail.com>
2026-04-15 13:21:49 -0400

    set angles for the next alignment run

--- java-utils/src/main/java/io/github/ftouchte/alignment/ResultsOverIterations.java
+++ java-utils/src/main/java/io/github/ftouchte/alignment/ResultsOverIterations.java
@@ -13,8 +13,13 @@ public class ResultsOverIterations {
     //public double[] layer_angles = {0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0};
     public double[] layer_residuals = {0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0};
 
-    public double[] layer_angles_start = {0.881, 1.023, 0.638, 0.905, 0.545, 0.680, 0.499, 0.567};
-    public double[] layer_angles_end = {0.881, 1.023, 0.638, 0.905, 0.545, 0.680, 0.499, 0.567};
+    // crystal ball v4 iter 15
+    // public double[] layer_angles_start = {0.881, 1.023, 0.638, 0.905, 0.545, 0.680, 0.499, 0.567};
+    // public double[] layer_angles_end = {0.881, 1.023, 0.638, 0.905, 0.545, 0.680, 0.499, 0.567};
+
+    // crystal ball v12 iter 12
+    public double[] layer_angles_start = {0.8998, 1.0507, 0.6151, 0.8525, 0.4530, 0.6205, 0.4035, 0.5831};
+    public double[] layer_angles_end   = {0.8774, 0.9017, 0.5051, 0.8746, 0.5643, 0.6194, 0.4645, 0.4064};
 
     public double[] layer_residuals_start = {0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0};
     public double[] layer_residuals_end = {0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0};
