542c8b9d673f5c4330f066e724fd06b5f24fc4b2
Felix Touchte Codjo <felixtouchtecodjo@gmail.com>
2026-05-07 11:47:51 -0400

    prepare to cook file from cache/ to test the new translation table

--- scripts/slurm/submit_slurm_jobs.sh
+++ scripts/slurm/submit_slurm_jobs.sh
@@ -1,19 +1,35 @@
 #!/bin/bash
 
+##########################################################################
+# Usage: better to read the code
+#      !!!! Need to modify the complementary code run.sbatch acoordingly
+##########################################################################
+
 # --- working directory
-workDir="/lustre24/expphy/volatile/clas12/touchte/alignment-cooking"
+workDir="/lustre24/expphy/volatile/clas12/touchte/new-translation-table" # should macth the one in run.sbatch
+cache="/cache/clas12/rg-l/data"
 
-# --- Location of the inputs files
-inputs="$workDir/decoded"
-outputs="$workDir/reconstructed"
+# --- Location of the reconstructed files
+decDir="$workDir/decoded"
+recDir="$workDir/reconstructed"
 
-input_list="$workDir/input_list.txt"
-output_list="$workDir/output_list.txt"
+if [[ ! -d "$decDir" ]]; then
+    mkdir "$decDir"
+fi
 
-# Create the list of input files
-find "$inputs" -regextype posix-extended -regex ".*/clas_[0-9]+\.evio\.[0-9]+\.hipo" | sort > "$input_list"
+if [[ ! -d "$recDir" ]]; then
+    mkdir "$recDir"
+fi
 
-# Create the list of output files
+evio_list="$workDir/evio_list.txt"
+dec_list="$workDir/dec_list.txt"
+rec_list="$workDir/rec_list.txt"
+
+# --- Create the list of input (here evio) files
+# find $cache/clas_02299[0124] -regextype posix-extended -regex ".*/clas_[0-9]+\.evio\.[0-9]+\.hipo" | sort > "$dec_list" # case of hipo file
+find $cache/clas_02299[0124] -regextype posix-extended -regex ".*/clas_[0-9]+\.evio\.[0-9]+" | sort > "$evio_list"
+
+# --- Create the list of dec files
 while read -r file
 do
     # Remove path for the file name
@@ -22,24 +38,55 @@ do
     if [[ "$filename" =~ clas_([0-9]+)\. ]]; then
         runno="${BASH_REMATCH[1]}"
     fi
-    # Print the output file in output_list.txt
-    echo "${outputs}/$runno/rec_${filename}"
-done < "$input_list" > "$output_list"
+    # create the repository for this run number if it does not exist
+    if [[ ! -d "$decDir/$runno" ]]; then 
+        mkdir "$decDir/$runno"
+    fi
+    # Print the name of the decoded file
+    echo "${decDir}/$runno/${filename}.hipo"
+done < "$evio_list" > "$dec_list"
+
+
+# --- Create the list of rec files
+while read -r file
+do
+    # Remove path for the file name
+    filename=$(basename "$file")
+    # extract the run number
+    if [[ "$filename" =~ clas_([0-9]+)\. ]]; then
+        runno="${BASH_REMATCH[1]}"
+    fi
+    # create the repository for this run number if it does not exist
+    if [[ ! -d "$recDir/$runno" ]]; then 
+        mkdir "$recDir/$runno"
+    fi
+    # Print the name of the reconstructed file
+    echo "${recDir}/$runno/rec_${filename}"
+done < "$dec_list" > "$rec_list"
 
 # Count number of files
-N=$(wc -l < "$input_list")
+N=$(wc -l < "$evio_list")
 
 echo "Number of files to be processed by SLURM:  $N"
 
 
 # !!! The user should use the files created here (input_list.txt and output_list.txt and more... in the script run.sbatch)
 
-# # Uncomment for testing
+# Uncomment for testing : deterministics
 # for i in 0 1 2 3
 # do
 #   export SLURM_ARRAY_TASK_ID=$i
 #   bash /w/hallb-scshelf2102/clas12/users/touchte/amon/scripts/slurm/run.sbatch
 # done
 
+# Uncomment for testing : random
+# for _ in 0 1 2 3
+# do
+#     export SLURM_ARRAY_TASK_ID=$((RANDOM % N))
+#     bash /w/hallb-scshelf2102/clas12/users/touchte/amon/scripts/slurm/run.sbatch
+# done
+
+
+
 # Uncomment to run command
-#sbatch --array=0-$((N-1)) /w/hallb-scshelf2102/clas12/users/touchte/amon/scripts/slurm/run.sbatch
+sbatch --array=0-$((N-1)) /w/hallb-scshelf2102/clas12/users/touchte/amon/scripts/slurm/run.sbatch
