a148d4d86d850c84409e1e5ce41e0981d91c61cc
Felix Touchte Codjo <felixtouchtecodjo@gmail.com>
2026-05-13 04:54:25 -0400

    add comments

--- scripts/hipo/event-filter.sh
+++ scripts/hipo/event-filter.sh
@@ -3,4 +3,7 @@
 # filter elastics event for one file
 # Make sure to modify the filtering criteria in io.github.ftouchte.filtering.ElasticEventFilter.java
 
+# $1 is the input filename
+# $2 is the output filename
+
 java -cp /w/hallb-scshelf2102/clas12/users/touchte/coatjava/coatjava/lib/clas/coat-libs-13.7.1-SNAPSHOT.jar:/w/hallb-scshelf2102/clas12/users/touchte/amon/java-utils/target/classes io.github.ftouchte.filtering.ElasticEventFilter $1 $2
