111e04f674f92fe29c6939af844eb14fb8c5e78d
Felix Touchte Codjo <felixtouchtecodjo@gmail.com>
2026-05-15 17:17:27 -0400

    cleaning and fix event computing time and rebin histos

--- java-utils/src/main/java/io/github/ftouchte/kalmanFilter/Histos.java
+++ java-utils/src/main/java/io/github/ftouchte/kalmanFilter/Histos.java
@@ -49,12 +49,12 @@ public class Histos {
         h1_phi0 = new H1F("expected_phi"  + tag, "expected phi", 100, 0, 360);
         h1_vz0 = new H1F("expected_vz"  + tag, "expected vz", 100, -24, 20);
 
-        h1_delta_p = new H1F("delta_p"  + tag, "delta p", 100, 0, 1000);
-        h1_delta_theta = new H1F("delta_theta"  + tag, "delta theta", 100, 0, 180);
-        h1_delta_phi = new H1F("delta_phi"  + tag, "delta phi", 100, 0, 360);
-        h1_delta_vz = new H1F("delta_vz"  + tag, "delta vz", 100, -24, 20);
+        h1_delta_p = new H1F("delta_p"  + tag, "delta p", 100, -500, 500);
+        h1_delta_theta = new H1F("delta_theta"  + tag, "delta theta", 100, -180, 180);
+        h1_delta_phi = new H1F("delta_phi"  + tag, "delta phi", 100, -40, 40);
+        h1_delta_vz = new H1F("delta_vz"  + tag, "delta vz", 100, -8, 20);
 
-        h1_computing_time = new H1F("computing_time", "ALERTEngine computing time", 100, 0, 0.5);
+        h1_computing_time = new H1F("computing_time" + tag, "ALERTEngine computing time", 100, 0, 40);
 
         /// --- title
         h1_computing_time.setTitleX("time (ms)");
