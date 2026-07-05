2650d25ec1308728c4079019f9503c1cf9b2f0c7
Felix Touchte Codjo <felixtouchtecodjo@gmail.com>
2026-07-05 13:13:52 -0400

    fix java path in the script

--- scripts/hipo/event-filter.sh
+++ scripts/hipo/event-filter.sh
@@ -6,4 +6,6 @@
 # $1 is the input filename
 # $2 is the output filename
 
-java -cp /w/hallb-scshelf2102/clas12/users/touchte/coatjava/coatjava/lib/clas/coat-libs-13.7.1-SNAPSHOT.jar:/w/hallb-scshelf2102/clas12/users/touchte/amon/java-utils/target/classes io.github.ftouchte.filtering.ElasticEventFilter $1 $2
+home_dir=/w/hallb-scshelf2102/clas12/users/touchte
+
+java -cp "$home_dir/coatjava/coatjava/lib/*:$home_dir/coatjava/coatjava/lib/clas/*:$home_dir/coatjava/coatjava/lib/services/*:$home_dir/coatjava/coatjava/lib/utils/*:$home_dir/amon/java-utils/target/classes" io.github.ftouchte.filtering.ElasticEventFilter $1 $2
